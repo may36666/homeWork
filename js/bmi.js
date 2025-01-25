6f7e321ba8e7f28278e8116e12b0bb041c180858
wayne <may36666@gmail.com>
2025-01-25 20:34:06 +0800

    bmi

--- js/bmi.js
+++ js/bmi.js
@@ -1,11 +1,20 @@
     let height = document.getElementById('height');
     let weight = document.getElementById('weight');
-    
+    let titleBmi = document.querySelectorAll('.titleBmi');
     let result = document.getElementById("result")
     
     result.addEventListener('click',function(e){
         height = height.value;
         weight = weight.value;
         let bmi = ((weight*10000) / Math.pow(height,2)).toFixed(2) ;
-        console.log(bmi);
+        let titleBmiLen =titleBmi.length;
+        for(i=0; i<titleBmiLen; i++){
+            titleBmi[i].textContent = bmi;
+        }
+        height.value ='';
+        weight.value ='';
+        
+
     })
+
+   
