bc1e68892d0d08fafab54571ddf27043f8c1d3f9
may36666 <may36666@gmail.com>
2025-01-25 22:44:39 +0800

    bmitotal

--- js/bmi.js
+++ js/bmi.js
@@ -4,17 +4,14 @@
     let result = document.getElementById("result")
     
     result.addEventListener('click',function(e){
-        height = height.value;
-        weight = weight.value;
-        let bmi = ((weight*10000) / Math.pow(height,2)).toFixed(2) ;
+        let heightVal = height.value;
+        let weightVal = weight.value;
+        let bmi = ((weightVal*10000) / Math.pow(heightVal,2)).toFixed(2) ;
+        console.log(bmi);
         let titleBmiLen =titleBmi.length;
         for(i=0; i<titleBmiLen; i++){
             titleBmi[i].textContent = bmi;
-        }
-        height.value ='';
-        weight.value ='';
-        
-
+        }   
     })
 
    
