44d7268882ba0e8ba4658fe3c038f80045baa072
may36666 <may36666@gmail.com>
2025-02-09 22:55:30 +0800

    alter travel bmi js

--- js/all.js
+++ js/all.js
@@ -1468,7 +1468,18 @@ let travel ={
     }
     }
 
-    
+    let travelZone =['--請選擇行政區--','前金區','新興區','鹽埕區','左營區','楠梓區','鼓山區','旗津區','苓雅區','三民區','前鎮區','小港區','鳳山區','鳥松區','大社區','仁武區','大樹區','岡山區','燕巢區','梓官區','永安區','彌陀區','橋頭區','田寮區','茄萣區','阿蓮區','路竹區','湖內區','那瑪夏區','桃源區','茂林區','六龜區','美濃區','旗山區','甲仙區','內門區','杉林區','林園區','大寮區區'];
+
+    let buttonZone =[
+        {Zone:'苓雅區',
+        Color:'bg-[#8A82CC]'},
+        {Zone:'三民區',
+        Color:'bg-[#FFA782]'},
+        {Zone:'新興區',
+        Color:'bg-[#F5D105]'},
+        {Zone:'鹽埕區',
+         Color:'bg-[#559AC8]'}
+        ];
 
     //旅遊網
     let area = document.getElementById('areaId');
@@ -1479,14 +1490,33 @@ let travel ={
     
 
 
+
     area.addEventListener('change',function (e){
         let select = e.target.value;
         mainTitleName.textContent = select;      
     },false);
 
- 
-
     area.addEventListener('change',updateMainList,false);
+
+    area.addEventListener('click',function(e){
+        let str = '';
+        let travelZoneLen = travelZone.length;
+        for(let i=0 ;i<travelZoneLen ; i++){
+            str+= '<option value="'+travelZone[i]+'">'+travelZone[i]+'</option>'
+        }
+        area.innerHTML = str;
+    })
+
+    
+    function hotButtonList(e){
+        let str ='';
+        let buttonZoneLen = buttonZone.length;
+        for(let i=0 ;i<buttonZoneLen ; i++){
+            str += '<button type="button" class="border-0 rounded-md text-white w-[20%] lg:w-[16%] h-full '+buttonZone[i].Color+'" value="'+buttonZone[i].Zone+'">'+ buttonZone[i].Zone+'</button>'
+        }
+        hotBotton.innerHTML = str ;
+    }
+    hotButtonList();
 
     
     function updateMainList(e){
--- js/bmi.js
+++ js/bmi.js
@@ -1,114 +1,95 @@
     let height = document.getElementById('height');
     let weight = document.getElementById('weight');
-    let titleBmi = document.querySelectorAll('.titleBmi');
-    let result = document.getElementById("result");
-    let resetBtn= document.querySelector('.resetBtn');
-    let standardWeight = document.getElementById('standardWeight');
-    let underWeight = document.getElementById('underWeight');
-    let fatWeight = document.getElementById('fatWeight');
-    let mildFatWeight = document.getElementById('mildFatWeight');
-    let moderatelyFatWeight =document.getElementById('moderatelyFatWeight');
-    let severeFatWeight = document.getElementById('severeFatWeight');
+    let result = document.getElementById('result');
+    let resultBtn = document.getElementById("resultBtn");
+    let titleWeight = document.getElementById('titleWeight');
     let bmi =0;
-    let standardResetBtn = document.getElementById('standardResetBtn')
-    let underResetBtn = document.getElementById('underResetBtn');
-    let fatResetBtn = document.getElementById('fatResetBtn');
-    let mildFatResetBtn = document.getElementById('mildFatResetBtn');
-    let moderatelyResetBtn = document.getElementById('moderatelyResetBtn');
-    let severeResetBtn = document.getElementById('severeResetBtn');
-
-    //計算BMI變更圖示
-    result.addEventListener('click',function(e){
-        bmiCountShow();
-        result.classList.toggle('hidden');
-        if(bmi<19){
-            underWeight.classList.toggle('hidden'); 
-        }
-        else if(bmi>=19 && bmi<24){
-            standardWeight.classList.toggle('hidden');
-        }
-        else if(bmi>=24 && bmi <27){
-            fatWeight.classList.toggle('hidden');
-        }
-        else if(bmi>=27 && bmi <30){
-            mildFatWeight.classList.toggle('hidden');
-        }
-        else if(bmi>=30 && bmi <35){
-            moderatelyFatWeight.classList.toggle('hidden');
-        }
-        else{
-            severeFatWeight.classList.toggle('hidden');
-        }
-        
-    })
-
-    function bmiCountShow(){
-        let heightVal = height.value;
-        let weightVal = weight.value;
-        bmi = ((weightVal*10000) / Math.pow(heightVal,2)).toFixed(2) ;
-        let titleBmiLen =titleBmi.length;
-        for(i=0; i<titleBmiLen; i++){
-            titleBmi[i].textContent = bmi;
-        }
-        
-    }
     
-    standardResetBtn.addEventListener('click',function(){
-        result.classList.toggle('hidden');
-        standardWeight.classList.toggle('hidden');  
-    })
-
-    underResetBtn.addEventListener('click',function(){
-        result.classList.toggle('hidden');
-        underWeight.classList.toggle('hidden');  
-    })
-
-    fatResetBtn.addEventListener('click',function(){
-        result.classList.toggle('hidden');
-        fatWeight.classList.toggle('hidden');  
-    })
-
-    mildFatResetBtn.addEventListener('click',function(){
-        result.classList.toggle('hidden');
-        mildFatWeight.classList.toggle('hidden');  
-    })
-
-    moderatelyResetBtn.addEventListener('click',function(){
-        result.classList.toggle('hidden');
-        moderatelyFatWeight.classList.toggle('hidden');  
-    })
-
-    severeResetBtn.addEventListener('click',function(){
-        result.classList.toggle('hidden');
-        severeFatWeight.classList.toggle('hidden');  
-    })
-
-    //紀錄BMI
 
     let bmiColor = [{
         BMI:"理想",
-        color:"bg-[#86D73F]"
+        color:"bg-[#86D73F]",
+        textColor:'text-[#86D73F]',
+        borserColor:'border-[#86D73F]'
     },{
         BMI:"過輕",
-        color:"bg-[#31BAF9]"
+        color:"bg-[#31BAF9]",
+        textColor:'text-[#31BAF9]',
+        borserColor:'border-[#31BAF9]'
     },
     {
         BMI:"過重",
-        color:"bg-[#FF982D]"
+        color:"bg-[#FF982D]",
+        textColor:'text-[#FF982D]',
+        borserColor:'border-[#FF982D]'
     },
     {
         BMI:"輕度肥胖",
-        color:"bg-[#FF6C03]"
+        color:"bg-[#FF6C03]",
+        textColor:'text-[#FF6C03]',
+        borserColor:'border-[#FF6C03]'
     },
     {
         BMI:"中度肥胖",
-        color:"bg-[#FF6C03]"
+        color:"bg-[#FF6C03]",
+        textColor:'text-[#FF6C03]',
+        borserColor:'border-[#FF6C03]'
     },
     {
         BMI:"重度肥胖",
-        color:"bg-[#FF1200]"
-    }
- ]
+        color:"bg-[#FF1200]",
+        textColor:'text-[#FF1200]',
+        borserColor:'border-[#FF1200]'
+    }];
+
+    //計算BMI變更圖示
+    resultBtn.addEventListener('click',function(e){
+        bmiCount();
+        console.log(bmi);
+        if(bmi<19){
+            result.innerHTML ='<div class="flex flex-row items-center  relative"id="titleWeight"> <div class="w-[120px] h-[120px] rounded-full border-[6px] border-[#86D73F] text-center items-center relative"><div class="text-[32px] text-[#86D73F] absolute left-[13px] top-[30px]"id="titleBmi"> </div><div class="text-[#86D73F] text-[14px] absolute left-[40px] bottom-[20px]">BMI</div><button type="button" class="w-[30px] h-[30px] bg-[#86D73F] rounded-full border-[3px] border-neutral-700 absolute bottom-0 right-0 z-50 hover:-rotate-45" id="ResetBtn"><img src="./assets/icons_loop.png" alt="" class="pl-[5px] contrast-200 "/></button></div><div class="text-[32px] text-[#86D73F] absolute right-[-80px]">理想</div></div>';
+            let titleBmi = document.getElementById('titleBmi');
+            let ResetBtn = document.getElementById('ResetBtn');
+            titleBmi.textContent = bmi;
+
+            ResetBtn.addEventListener('click',function(){
+                result.innerHTML ='<button type="button" id="resultBtn"><div class="w-[100px] h-[100px] rounded-full bg-[#FFD366] hover:shadow-[0_1px_6px_3px] hover:shadow-amber-200/50 active:bg-[#DEA821] text-center text-[25px] align-middle py-[35%]">看結果</div></button>';
+            })
+        }
+        else if(bmi>=19 && bmi<24){
+            
+        }
+        else if(bmi>=24 && bmi <27){
+            
+        }
+        else if(bmi>=27 && bmi <30){
+           
+        }
+        else if(bmi>=30 && bmi <35){
+            
+        }
+        else{
+            
+        }
+        
+        
+        
+    });
+
+    function bmiCount(){
+        let heightVal = height.value;
+        let weightVal = weight.value;
+        bmi = ((weightVal*10000) / Math.pow(heightVal,2)).toFixed(2) ;
+    };
+
+    
+    
+    
+    
+
+    //紀錄BMI
+
+    
     let bmiList = document.getElementById('bmiList');
     let data = JSON.parse(localStorage.getItem('listData')) || [];
     
