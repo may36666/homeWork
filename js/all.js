1305faa5f2cf1997c99eb660d81da0ea0d51ad3c
may36666 <may36666@gmail.com>
2025-02-18 01:43:10 +0800

    travel ajax

--- js/all.js
+++ js/all.js
@@ -1,38 +1,31 @@
+let buttonZone =[
+    {Zone:'苓雅區',
+    Color:'bg-[#8A82CC]'},
+    {Zone:'三民區',
+    Color:'bg-[#FFA782]'},
+    {Zone:'新興區',
+    Color:'bg-[#F5D105]'},
+    {Zone:'鹽埕區',
+     Color:'bg-[#559AC8]'}
+    ];
+    //用set
+    let travelZone =['--請選擇行政區--','前金區','新興區','鹽埕區','左營區','楠梓區','鼓山區','旗津區','苓雅區','三民區','前鎮區','小港區','鳳山區','鳥松區','大社區','仁武區','大樹區','岡山區','燕巢區','梓官區','永安區','彌陀區','橋頭區','田寮區','茄萣區','阿蓮區','路竹區','湖內區','那瑪夏區','桃源區','茂林區','六龜區','美濃區','旗山區','甲仙區','內門區','杉林區','林園區','大寮區'];
 
-let travel ={};
 
-  
 let xhr = new XMLHttpRequest();
 xhr.open('get','https://raw.githubusercontent.com/hexschool/KCGTravel/master/datastore_search.json',true);
 xhr.send(null);
 xhr.onload =function (){
-    travel =JSON.parse(xhr.responseText);
-    console.log(travel);
-}
+    let travel =JSON.parse(xhr.responseText);
 
-
-    let travelZone =['--請選擇行政區--','前金區','新興區','鹽埕區','左營區','楠梓區','鼓山區','旗津區','苓雅區','三民區','前鎮區','小港區','鳳山區','鳥松區','大社區','仁武區','大樹區','岡山區','燕巢區','梓官區','永安區','彌陀區','橋頭區','田寮區','茄萣區','阿蓮區','路竹區','湖內區','那瑪夏區','桃源區','茂林區','六龜區','美濃區','旗山區','甲仙區','內門區','杉林區','林園區','大寮區區'];
-
-    let buttonZone =[
-        {Zone:'苓雅區',
-        Color:'bg-[#8A82CC]'},
-        {Zone:'三民區',
-        Color:'bg-[#FFA782]'},
-        {Zone:'新興區',
-        Color:'bg-[#F5D105]'},
-        {Zone:'鹽埕區',
-         Color:'bg-[#559AC8]'}
-        ];
-
-    //旅遊網
+    if(xhr.status == 200 && xhr.readyState == 4){
     let area = document.getElementById('areaId');
     let mainTitleName = document.getElementById('mainTitleName');
     let travelLen = travel.result.records.length;
     let mainListId = document.getElementById('mainListId');
-    let hotBotton = document.getElementById('hotButton')
+    let hotBotton = document.getElementById('hotButton');
+
     
-
-
 
     area.addEventListener('change',function (e){
         let select = e.target.value;
@@ -45,7 +38,7 @@ xhr.onload =function (){
         let str = '';
         let travelZoneLen = travelZone.length;
         for(let i=0 ;i<travelZoneLen ; i++){
-            str+= '<option value="'+travelZone[i]+'">'+travelZone[i]+'</option>'
+            str+= '<option value="'+travelZone[i]+'"> '+travelZone[i]+' </option>'
         }
         area.innerHTML = str;
     })
@@ -80,7 +73,11 @@ xhr.onload =function (){
     },false);
 
     hotBotton.addEventListener('click',updateMainList);
+    }
+    else{
+        console.log('資料錯誤');
+    }
+}
 
-  
-    
+
     
